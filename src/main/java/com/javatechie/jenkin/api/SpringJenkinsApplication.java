1df9c46f7b17cb7ed6c6d88c6feffeb33ac219a6
ayyazk1 <ayyaz.khan@bellurbis.com>
2022-11-15 15:57:24 +0530

    create controller in main class

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -3,16 +3,16 @@ package com.javatechie.jenkin.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringJenkinsApplication {
-
-
-	@GetMapping("/ping")
-	public String message() {
-		return "Wao!! Application Deployed successfully in SAP Cloud..";
+	@GetMapping("/home/{name}")
+	public String hello0(@PathVariable("name") String name)
+	{
+		return "Hello, Welcome To Bellurbis Technologies  " + name;
 	}
 
 	public static void main(String[] args) {
